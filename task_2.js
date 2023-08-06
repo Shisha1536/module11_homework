dbcc8663ade3f6fe2e606f665834101840736bf0
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2023-08-06 21:38:24 +0300

    task_2

---
+++ task_2.js
@@ -0,0 +1,16 @@
+function numPrime(){
+    let num = prompt();
+    debugger
+    if(typeof +num === `number` && +num > 1 && +num < 1000){
+        for(let i = 2; i < +num; i++){
+            if(+num % i === 0){
+                return "число составное";
+            } else{
+                return "число простое";
+            }
+        }
+    } else{
+        return "данные неверны";
+    }
+}
+console.log(numPrime());
